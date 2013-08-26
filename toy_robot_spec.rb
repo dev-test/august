15fba77336e481831e890159fa145f7113e93ad2
jpaul <jpaul@dius.com.au>
2013-08-26 16:53:35 +1000

    First initial test

--- toy_robot_spec.rb
+++ toy_robot_spec.rb
@@ -1,5 +1,11 @@
 require 'toy_robot'
 
 describe ToyRobot do
+  context "when initialized" do
+    it "is facing north" do
+      robot = ToyRobot.new
+      expect(robot.facing).to eq("north")
+    end
+  end
 
 end
