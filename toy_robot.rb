ab9d3505fc4364669403bdf0fb4c3b1ae5a65227
jpaul <jpaul@dius.com.au>
2013-08-26 20:13:36 +1000

    I'm not happy with the definition of place

--- toy_robot.rb
+++ toy_robot.rb
@@ -1,4 +1,5 @@
 class ToyRobot
+  ORIGIN = [0,0]
 
   def facing 
     @facing
@@ -14,6 +15,15 @@ class ToyRobot
   end
 
   def report
+    return unless defined? @position
+    "#{@position.inspect}, #{@facing}"
   end
+  
+  def place(position=nil, facing=nil)
+    @position = position
+    @facing = facing
 
+    @position = ToyRobot::ORIGIN unless @position
+    @facing = :NORTH unless @facing
+  end
 end
--- toy_robot_spec.rb
+++ toy_robot_spec.rb
@@ -1,43 +1,50 @@
 require '/Users/jeromepaul/locomote/toy_robot.rb'
 
 describe ToyRobot do
-  context "when initialized" do
-    before(:each) do
-      @robot = ToyRobot.new
-    end
-
-    context "before place is called" do 
-      it "should ignore move" do
-        @robot.move.should be_nil
-      end
-      it "should ignore left" do
-        @robot.left.should be_nil
-      end
-      it "should ignore right" do
-        @robot.right.should be_nil
-      end
-      it "should ignore report" do
-        @robot.report.should be_nil
-      end
-    end
-
-    describe "place" do
-
-      describe "with no params" do
-        it "should place the robot in position 0,0" do
-          @robot.place
-          @robot.report.should eq ToyRobot::origin 
-        end
-      end
-
-      [[0,1],[1,0],[3,4]].each{|position|
-        describe "with valid param #{position}" do
-          @robot.place(position)         
-          @robot.report.should eq position
-        end  
-      }
-    end
-
+  before(:each) do
+    @robot = ToyRobot.new
   end
 
+  context "before place is called" do 
+    it "should ignore move" do
+      @robot.move.should be_nil
+    end
+    it "should ignore left" do
+      @robot.left.should be_nil
+    end
+    it "should ignore right" do
+      @robot.right.should be_nil
+    end
+    it "should ignore report" do
+      @robot.report.should be_nil
+    end
+  end
+
+  describe "place" do
+
+    describe "with no params" do
+      it "should place the robot in position 0,0 facing NORTH" do
+        @robot.place
+        @robot.report.should eq "#{ToyRobot::ORIGIN.inspect}, NORTH" 
+      end
+    end
+
+    [[0,1],[1,0],[3,4]].each{|position|
+      describe "with valid param #{position} facing SOUTH" do
+        it "should place the robot in that tile facing SOUTH" do
+          @robot.place(position, :SOUTH)         
+          @robot.report.should eq "#{position.inspect}, SOUTH"
+        end
+      end  
+    }
+
+   [[-1,4], [0,5]].each{|position|
+     describe "with invalid params #{position}" do 
+       it "should ignore the place" do
+         @robot.place(position, :NORTH)
+         @report.should be_nil
+       end
+     end
+   }
+  end
 end
