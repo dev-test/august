0a643e2d707f2fd3fd8de321c58c5e34611a8168
jpaul <jpaul@dius.com.au>
2013-08-27 12:59:41 +1000

    added the runnable script and fixed a defect where facing was not being coverted to a sym

---
+++ robot_runner.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/env ruby
+require_relative "toy_robot"
+
+robot = ToyRobot.new
+
+while input = gets
+  case input
+  when /^PLACE$/i
+    robot.place
+  when /^PLACE \d, \d, NORTH|SOUTH|EAST|WEST$/
+    data = input.match(/^PLACE (\d),(\d),(\w*)$/i)
+    robot.place([data[1].to_i, data[2].to_i], data[3]) 
+  when /^MOVE$/i
+    robot.move
+  when /^LEFT$/i
+    robot.left
+  when /^RIGHT$/i
+    robot.right
+  when /^REPORT$/i
+    puts robot.report
+  when /^EXIT$/i
+    exit 
+  else
+    puts "unknown command, valid commands are: PLACE, 'PLACE x,y,facing', MOVE, LEFT, RIGHT, REPORT or EXIT"
+  end
+end 
